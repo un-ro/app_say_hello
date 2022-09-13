6e4a0fc7edcf4ebb0a82a4675afb981998fdfadc
Unero <ramadhanbhg@gmail.com>
2022-09-13 08:20:38 +0700

    main

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/un-ro/app_say_hello
+
+go 1.19
+
+require github.com/un-ro/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/un-ro/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
